e3f655c51fda805e95c474e1b897f761e0130f0d
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-01-01 11:15:48 +0300

    Working on widget

--- src/components/widgets/large/WidgetLarge.jsx
+++ src/components/widgets/large/WidgetLarge.jsx
@@ -1,5 +1,17 @@
 import "./WidgetLarge.css";
 
 export default function WidgetLarge() {
-  return <div>Large Widget</div>;
+  return (
+    <div className="widget-large">
+      <h3 className="widget-large-title">Latest transactions</h3>
+      <table className="widget-large-table">
+        <tr className="widget-large-table-row">
+          <th className="widget-large-table-head">Customers</th>
+          <th className="widget-large-table-head">Date</th>
+          <th className="widget-large-table-head">Amount</th>
+          <th className="widget-large-table-head">Status</th>
+        </tr>
+      </table>
+    </div>
+  );
 }
--- src/components/widgets/small/WidgetSmall.jsx
+++ src/components/widgets/small/WidgetSmall.jsx
@@ -1,5 +1,102 @@
+import { Visibility } from "@mui/icons-material";
 import "./WidgetSmall.css";
 
 export default function WidgetSmall() {
-  return <div>Small widget</div>;
+  return (
+    <div className="widget-small">
+      <span className="widget-small-title">New Join Members</span>
+      <ul className="widget-small-list">
+        <li className="widget-small-list-item">
+          <img
+            className="widget-small-image"
+            src="images/Ojok.jpg"
+            alt="User Image"
+          />
+          <div className="widget-small-user">
+            <span className="widget-small-username">Ojok Simon Peter</span>
+            <span className="widget-small-user-title">Software Engineer</span>
+          </div>
+          <button className="widget-small-btn">
+            <Visibility className="widget-small-icon" />
+            Display
+          </button>
+        </li>
+        <li className="widget-small-list-item">
+          <img
+            className="widget-small-image"
+            src="images/Ojok.jpg"
+            alt="User Image"
+          />
+          <div className="widget-small-user">
+            <span className="widget-small-username">Ojok Simon Peter</span>
+            <span className="widget-small-user-title">Software Engineer</span>
+          </div>
+          <button className="widget-small-btn">
+            <Visibility className="widget-small-icon" />
+            Display
+          </button>
+        </li>
+        <li className="widget-small-list-item">
+          <img
+            className="widget-small-image"
+            src="images/Ojok.jpg"
+            alt="User Image"
+          />
+          <div className="widget-small-user">
+            <span className="widget-small-username">Ojok Simon Peter</span>
+            <span className="widget-small-user-title">Software Engineer</span>
+          </div>
+          <button className="widget-small-btn">
+            <Visibility className="widget-small-icon" />
+            Display
+          </button>
+        </li>
+        <li className="widget-small-list-item">
+          <img
+            className="widget-small-image"
+            src="images/Ojok.jpg"
+            alt="User Image"
+          />
+          <div className="widget-small-user">
+            <span className="widget-small-username">Ojok Simon Peter</span>
+            <span className="widget-small-user-title">Software Engineer</span>
+          </div>
+          <button className="widget-small-btn">
+            <Visibility className="widget-small-icon" />
+            Display
+          </button>
+        </li>
+        <li className="widget-small-list-item">
+          <img
+            className="widget-small-image"
+            src="images/Ojok.jpg"
+            alt="User Image"
+          />
+          <div className="widget-small-user">
+            <span className="widget-small-username">Ojok Simon Peter</span>
+            <span className="widget-small-user-title">Software Engineer</span>
+          </div>
+          <button className="widget-small-btn">
+            <Visibility className="widget-small-icon" />
+            Display
+          </button>
+        </li>
+        <li className="widget-small-list-item">
+          <img
+            className="widget-small-image"
+            src="images/Ojok.jpg"
+            alt="User Image"
+          />
+          <div className="widget-small-user">
+            <span className="widget-small-username">Ojok Simon Peter</span>
+            <span className="widget-small-user-title">Software Engineer</span>
+          </div>
+          <button className="widget-small-btn">
+            <Visibility className="widget-small-icon" />
+            Display
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
