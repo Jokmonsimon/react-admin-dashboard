89fb1a346608ef3ab0043dc65973229142f1ef01
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-01-02 07:18:13 +0300

    Added route using to App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import Header from "./components/header/Header";
 import Sidebar from "./components/sidebar/Sidebar";
 import "./App.css";
 import Dashboard from "./pages/dashboard/Dashboard";
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
       <div className="sidebar-container">
         <Sidebar />
-        <Dashboard />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="users" element={<UserList />} />
+        </Routes>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
