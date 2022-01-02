719f497457b3dd6b75d51d22691e5319c358d5bf
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-01-02 13:53:16 +0300

    New update

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,3 +1,4 @@
+import { Add } from "@mui/icons-material";
 import "./User.css";
 
 export default function User() {
@@ -5,10 +6,16 @@ export default function User() {
     <div className="user">
       <div className="user-title-container">
         <h1 className="user-title">Edit User</h1>
-        <button className="add-user-button">Add User</button>
+        <button className="add-user-button">
+          <Add /> Add User
+        </button>
       </div>
       <div className="user-container">
-        <div className="show-user"></div>
+        <div className="show-user">
+          <img src="/images/Ojok.jpg" alt="" className="show-user-iamge" />
+          <div className="show-username">Ojok Simon Peter</div>
+          <div className="show-user-title">Full-stack Developer</div>
+        </div>
         <div className="update-user"></div>
       </div>
     </div>
