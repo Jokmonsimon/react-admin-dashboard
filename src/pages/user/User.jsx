4dec0a01cf36fe5e04e651c965c1c3c2de7fd182
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-01-02 19:44:56 +0300

    Updated user account details

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -30,23 +30,23 @@ export default function User() {
             <span className="show-user-account-title">Account Details</span>
             <div className="show-user-info">
               <PermIdentityIcon className="show-user-icon" />
-              <div className="show-username">jokmonaimon</div>
+              <div className="show-user-detail-info">jokmonsimon</div>
             </div>
             <div className="show-user-info">
               <CalendarToday className="show-user-icon" />
-              <div className="show-username">03 June 1993</div>
+              <div className="show-user-detail-info">03 June 1993</div>
             </div>
             <div className="show-user-info">
               <PhoneAndroid className="show-user-icon" />
-              <div className="show-username">+256 776 020 431</div>
+              <div className="show-user-detail-info">+256 776 020 431</div>
             </div>
             <div className="show-user-info">
               <MailOutlined className="show-user-icon" />
-              <div className="show-username">sp.jokmon@gmail.com</div>
+              <div className="show-user-detail-info">sp.jokmon@gmail.com</div>
             </div>
             <div className="show-user-info">
               <LocationCityOutlined className="show-user-icon" />
-              <div className="show-username">Gulu | Uganda</div>
+              <div className="show-user-detail-info">Gulu | Uganda</div>
             </div>
           </div>
         </div>
