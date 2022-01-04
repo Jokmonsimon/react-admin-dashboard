6b8cff3bc8689555373d41236b0ad1d3f0d60451
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-01-04 13:14:02 +0300

    Updated

---
+++ src/pages/productList/ProductList.jsx
@@ -0,0 +1,9 @@
+import "./ProductList.css";
+
+export default function ProductList() {
+  return (
+    <div className="product-list">
+      <h1 className="product-title">Product List</h1>
+    </div>
+  );
+}
