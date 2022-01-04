431812f8a7464aa81ec4c1fdcac7e6b70809878e
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-01-04 13:17:34 +0300

    New update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
-import ProductList from "./pages/productList/Product";
+import ProductList from "./pages/productList/ProductList";
 
 function App() {
   return (
@@ -20,6 +20,8 @@ function App() {
           <Route path="/user/:id" element={<User />} />
           <Route path="/addNewUser" element={<NewUser />} />
           <Route path="/products" element={<ProductList />} />
+          <Route path="/product/:id" element={<User />} />
+          <Route path="/addNewProduct" element={<NewUser />} />
         </Routes>
       </div>
     </BrowserRouter>
