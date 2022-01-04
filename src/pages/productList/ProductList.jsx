85f8ebd6c0ff10ea3467dadd76b5eb71ba5c72a7
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-01-04 14:09:41 +0300

    New update

--- src/pages/productList/ProductList.jsx
+++ src/pages/productList/ProductList.jsx
@@ -1,9 +1,22 @@
 import "./ProductList.css";
+import { DataGrid } from "@mui/x-data-grid";
+import { DeleteOutlined, Edit } from "@mui/icons-material";
+import { userRows } from "../../data";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function ProductList() {
   return (
     <div className="product-list">
       <h1 className="product-title">Product List</h1>
+      <DataGrid
+        disableSelectionOnClick
+        rows={data}
+        columns={columns}
+        pageSize={9}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
     </div>
   );
 }
