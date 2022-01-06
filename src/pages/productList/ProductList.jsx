3e99c812cb9b454d7f818d88d601b2c11e2e21dc
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-01-06 23:44:56 +0300

    Update

--- src/pages/productList/ProductList.jsx
+++ src/pages/productList/ProductList.jsx
@@ -1,11 +1,63 @@
 import "./ProductList.css";
 import { DataGrid } from "@mui/x-data-grid";
 import { DeleteOutlined, Edit } from "@mui/icons-material";
-import { userRows } from "../../data";
+import { productRows } from "../../data";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
 export default function ProductList() {
+  const [data, setData] = useState(productRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
+  const columns = [
+    { field: "productId", headerName: "ID", width: 40 },
+    {
+      field: "user",
+      headerName: "User",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="user-list-user">
+            <img src={params.row.avatar} alt="" className="user-list-image" />
+            {params.row.username}
+          </div>
+        );
+      },
+    },
+    { field: "email", headerName: "Email", width: 200 },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 120,
+    },
+    {
+      field: "transaction",
+      headerName: "Transaction",
+      width: 150,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={"/user/" + params.row.id} className="link">
+              <Edit className="user-list-edit" />
+            </Link>
+            <DeleteOutlined
+              className="user-list-delete"
+              onClick={() => handleDelete(params.row.id)}
+            />
+          </>
+        );
+      },
+    },
+  ];
+
   return (
     <div className="product-list">
       <h1 className="product-title">Product List</h1>
