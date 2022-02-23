8d9c2e06db7c7d31765127945c79eb6b2ef894cc
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-02-23 09:31:30 +0300

    New update

---
+++ src/pages/transaction/TransactionList.jsx
@@ -0,0 +1,9 @@
+import "./TransactionList.css";
+
+export default function TransactionList() {
+  return (
+    <div className="transaction-list">
+      <h1>All Transactions Coming Soon!</h1>
+    </div>
+  );
+}
